947ee5fe76992a4322687f6c98f17cd05b151b75
tazoakhalaia <darknemiroff@gmail.com>
2024-02-10 22:41:47 +0400

    add platform

--- src/game/classes/main.class.ts
+++ src/game/classes/main.class.ts
@@ -1,16 +1,23 @@
 import { Container, Graphics } from 'pixi.js';
-import { Car } from './car.class.';
 import { Map } from './map/map.class';
+import { PlatformOne } from './levels/roundOne/level-one.class';
 
 export class GameComponent {
   private _container = new Container();
   private map = new Map();
+  private platformOne = new PlatformOne()
 
   get container() {
     return this._container;
   }
   init() {
-    this.map.init();
-    this._container.addChild(this.map.container);
+    // this.map.init();
+    // this.map.callback = (value: any) => {
+    //   console.log(value);
+    //   value.garageContainer.removeChildren();
+    // };
+    // this._container.addChild(this.map.container);
+    this.platformOne.init()
+    this._container.addChild(this.platformOne.container)
   }
 }
--- src/game/classes/map/map.class.ts
+++ src/game/classes/map/map.class.ts
@@ -3,13 +3,23 @@ import { Assets, Container, Graphics, Sprite, Text } from 'pixi.js';
 export class Map {
   private readonly CANVAS_WIDTH = 1500;
   private readonly CANVAS_HEIGHT = 700;
-  private readonly GARAGE_CON_WIDTH = 400
+  private readonly GARAGE_CON_WIDTH = 400;
   private _container = new Container();
   private map: Sprite | undefined;
   private menuContainer = new Container();
-  private garageContainer = new Container()
+  private garageContainer = new Container();
+  private textC = new Container();
+  private _callback = (value: any) => undefined;
   get container() {
     return this._container;
+  }
+
+  get callback() {
+    return this._callback;
+  }
+
+  set callback(value: any) {
+    this._callback = value;
   }
 
   init() {
@@ -17,19 +27,21 @@ export class Map {
     this.menuContainer.height = 90;
     this.menuContainer.x = 40;
     this.menuContainer.y = 25;
-    this.garageContainer.width = this.GARAGE_CON_WIDTH
-    this.garageContainer.height = 90
-    this.garageContainer.x = 1062
-    this.garageContainer.y = 25
+    this.garageContainer.width = this.GARAGE_CON_WIDTH;
+    this.garageContainer.height = 90;
+    this.garageContainer.x = 1062;
+    this.garageContainer.y = 25;
     this.renderMap();
     this.mapCBorder();
     this.mapTextC();
     this.mapText();
-    this.garageBorder()
-    this.garage()
-    this.rightArrow()
-    this.garageText()
-    this.garageClick()
+    this.garageBorder();
+    this.garage();
+    this.rightArrow();
+    this.garageText();
+    this.garageClick();
+    this.letsPlayText();
+    this.playText();
   }
 
   renderMap() {
@@ -63,7 +75,6 @@ export class Map {
     this._container.addChild(this.menuContainer);
   }
 
-
   mapText() {
     const menu = new Text('Map', {
       fontSize: 46,
@@ -83,8 +94,8 @@ export class Map {
     background.endFill();
     background.x = this.garageContainer.width / 2 - background.width / 2;
     background.y = this.garageContainer.height / 2 - background.height / 2;
-    this.garageContainer.addChild(background)
-    this._container.addChild(this.garageContainer)
+    this.garageContainer.addChild(background);
+    this._container.addChild(this.garageContainer);
   }
 
   garageBorder() {
@@ -92,40 +103,60 @@ export class Map {
     border.beginFill('#cbab7a');
     border.drawRoundedRect(0, 0, 415, 85, 30);
     border.endFill();
-    this.garageContainer.addChild(border)
-    this._container.addChild(this.garageContainer)
+    this.garageContainer.addChild(border);
+    this._container.addChild(this.garageContainer);
   }
 
   rightArrow() {
     const arrow = new Sprite(Assets.get('rightArrow'));
-    arrow.width = 50
-    arrow.height = 45
-    arrow.position.set(
-      30,
-      this.garageContainer.height / 2 - arrow.height  / 2
-    )
-    this.garageContainer.addChild(arrow)
+    arrow.width = 50;
+    arrow.height = 45;
+    arrow.position.set(30, this.garageContainer.height / 2 - arrow.height / 2);
+    this.garageContainer.addChild(arrow);
   }
 
   garageText() {
     const text = new Text('Garage', {
       fontSize: 46,
-      fill: '#fff'
-    })
+      fill: '#fff',
+    });
     text.position.set(
       this.garageContainer.width / 2 - text.width / 2,
       this.garageContainer.height / 2 - text.height / 2
-    )
-    this.garageContainer.addChild(text)
+    );
+    this.garageContainer.addChild(text);
   }
 
   garageClick = () => {
-    this.garageContainer.eventMode = 'dynamic'
-    this.garageContainer.cursor = 'pointer'
+    this.garageContainer.eventMode = 'dynamic';
+    this.garageContainer.cursor = 'pointer';
     this.garageContainer.addEventListener('pointerup', () => {
-      console.log('garage');
-      
-    })
+      this._callback({
+        garageContainer: this._container,
+      });
+    });
+  };
+
+  letsPlayText() {
+    this.textC = new Container();
+    this.textC.y = 585;
+    this.textC.x = 40;
+    const textG = new Graphics();
+    textG.beginFill('#8c7655');
+    textG.drawRoundedRect(0, 0, 370, 80, 20);
+    this.textC.addChild(textG);
+    this._container.addChild(this.textC);
   }
 
+  playText() {
+    const haveFun = new Text('Have Fun', {
+      fontSize: 46,
+      fill: '#fff',
+    });
+    haveFun.position.set(
+      this.textC.width / 2 - haveFun.width / 2,
+      this.textC.height / 2 - haveFun.height / 2
+    );
+    this.textC.addChild(haveFun);
+  }
 }
